a0b3519a8c96b38ad1b4dd62d36f429837c71817
sohammondal <soham.pf.mondal@gmail.com>
2021-07-16 23:56:01 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {Text} from 'react-native';
+
+import {Container} from './styles';
+
+export default function App() {
+  return (
+    <Container>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </Container>
+  );
+}
